6272e7cdbf8fc9464e65cb7d33b847e62e750b95
Lena BAMOUH <lenabamoh@gmail.com>
2023-04-26 12:17:21 +0200

    Parsed file and run method

--- src/language-server/main.ts
+++ src/language-server/main.ts
@@ -1,9 +1,8 @@
-import { EmptyFileSystem, Reference, toString } from 'langium';
-import { parseHelper } from 'langium/test';
-import { extractDocument } from '../cli/cli-util';
-import { PetriNet, Place, Transition, isArcPtT, isEvolution } from './generated/ast';
+import { extractAstNode } from '../cli/cli-util';
+import { PetriNet } from './generated/ast';
 import { createPetriNetServices } from './petri-net-module';
-import { ArcPtTState, ArcTtPState, EvolutionState, PetriNetState, PlaceState, ResetState, TokenState, TransitionState, Trigger } from './runtimeState';
+import { PetriNetState } from './runtimeState';
+import { NodeFileSystem } from 'langium/node';
 
 /* // Create a connection to the client
 const connection = createConnection(ProposedFeatures.all);
@@ -14,81 +13,10 @@ const { shared } = createPetriNetServices({ connection, ...NodeFileSystem });
 // Start the language server with the shared services
 startLanguageServer(shared); */
 
-/**
- * Create a PlaceState from a reference
- * 
- * @param petrinet, the parsed petrinet
- * @param refPlace, the place's reference
- * @param petrinetState, the petrinetState corresponding to the the parsed petrinet
- * @returns new PlaceState with the attributes of the reference
- */
-function findPlaceFromReference(petrinet: PetriNet, refPlace: Reference<Place>, petrinetState: PetriNetState): PlaceState {
-  for(let place of petrinet.places) {
-    if(place === refPlace.ref) return new PlaceState(petrinetState, place.name, place.maxCapacity, place.initialTokenNumber);
-  }
-  return new PlaceState(petrinetState, "null", 0);
+async function parse(fileName: string) {
+  const services = createPetriNetServices(NodeFileSystem).PetriNet;
+  return await extractAstNode<PetriNet>(fileName, services);
 }
-
-/**
- * Create a TransitionState from a reference
- * 
- * @param petrinet, the parsed petrinet
- * @param refTransition, the transition's reference
- * @param petrinetState, the petrinetState corresponding to the the parsed petrinet
- * @returns new TransitionState with the attributes of the reference
- */
-function findTransitionFromReference(petrinet: PetriNet, refTransition: Reference<Transition>, petrinetState: PetriNetState): TransitionState {
-  for(let transition of petrinet.transitions) {
-    if(transition === refTransition.ref) return new TransitionState(petrinetState, transition.name);
-  }
-  return new TransitionState(petrinetState, "null");
-}
-
-/**
- * Will continue to trigger transitions until there are no doable transitions or
- * that the max iteration is reached
- * 
- * @param i is the number of times the petri net has been evolving
- */
-function EvolveIt(petrinetState:PetriNetState, i: number, tokens: Array<TokenState>, everyTriggers: Array<Trigger>): number {
-  for (let transition of petrinetState.getTransitions()) {
-    if (petrinetState.transitionDoAble(transition))
-      everyTriggers.push(new Trigger(petrinetState, true, transition, tokens));
-  }
-  return i + 1;
-}
-
-/**
- * Verifies that the petri net can evolve
- * 
- * @param i is the number of times the petri net has been evolving
-*/
-function ContinueEvolving(petrinetState: PetriNetState, i: number, tokens: Array<TokenState>, everyTriggers: Array<Trigger>): void {
-  if ((petrinetState.canEvolve()) || (i < petrinetState.getMaxIterations()))
-    EvolveIt(petrinetState, i, tokens, everyTriggers);
-}
-
-  /**
-   * Will continue to cancel triggers of an Evolution
-   * 
-   * @param n is the number of events contained in the petrinet
-   */
-  function continueResetting(petrinetState: PetriNetState, sizeEvents: number, tokens: Array<TokenState>): void {
-    if (sizeEvents > 0) {
-        let currentEvent = sizeEvents - 1;
-            if (petrinetState.getEvents()[sizeEvents-1] instanceof EvolutionState) {
-                while (petrinetState.getEvents()[currentEvent].getEveryTriggers().length !== 0) {
-                    new Trigger(petrinetState, false, petrinetState.getEvents()[currentEvent].getEveryTriggers()[0].getTransition(), tokens);
-                    petrinetState.getEvents()[currentEvent].removeTrigger(
-                            petrinetState.getEvents()[currentEvent].getEveryTriggers()[0]);
-                }
-            }
-        petrinetState.removeEvent(petrinetState.getEvents()[currentEvent]);
-      if (petrinetState.getEvents()[sizeEvents-1] instanceof ResetState) 
-        petrinetState.removeEvent(petrinetState.getEvents()[currentEvent]);
-      continueResetting(petrinetState, petrinetState.getEvents().length, tokens);
-    }
-  }
 
 /**
  * Will run the program with the informations of an existing file.
@@ -97,113 +25,59 @@ function ContinueEvolving(petrinetState: PetriNetState, i: number, tokens: Array
  */
 async function run(fileName: string): Promise<void> {
 
-  const services = createPetriNetServices(EmptyFileSystem).PetriNet;
-  const document = await extractDocument(fileName, services);
-  const parse = parseHelper<PetriNet>(services);
-  const ast = await parse(toString(document));
-  const petrinet = ast.parseResult.value;
+  const petrinet = parse(fileName);
+  let petrinetState: PetriNetState = new PetriNetState(await petrinet, 300);
 
-  let petrinetState: PetriNetState = new PetriNetState(petrinet.name, 300);
 
-// Creation of the arcs in PetriNetState
-for(let arc of petrinet.arcs) {
-  console.log(arc.source.$refText);
-    if(isArcPtT(arc)) {
-        new ArcPtTState(petrinetState, arc.name, findPlaceFromReference(petrinet, arc.source, petrinetState), findTransitionFromReference(petrinet, arc.target, petrinetState), arc.weight);
-    } else {
-        new ArcTtPState(petrinetState, arc.name, findTransitionFromReference(petrinet, arc.source, petrinetState), findPlaceFromReference(petrinet, arc.target, petrinetState), arc.weight);
+  console.log("------------------------------------------------------------------------");
+
+  console.log("1 : Base");
+  for (let place of petrinetState.getPlaces()) {
+    console.log();
+    console.log("    Tokens in place " + place.getPlace().name + " : ");
+    for (let i = 0; i < petrinetState.getTokens().length; i++) {
+      if (petrinetState.getTokens()[i].getPosition() == place) {
+        console.log(petrinetState.getTokens()[i] + " , ");
+      }
     }
-}
-
-// Creation of the places in PetriNetState
-for(let place of petrinet.places) {
-  if(!petrinetState.containsPlace(place.name))
-    new PlaceState(petrinetState, place.name, place.maxCapacity, place.initialTokenNumber);
-}
-
-// Creation of the transitions in PetriNetState
-for(let transition of petrinet.transitions) {
-  if(!petrinetState.containsTransition(transition.name))
-    new TransitionState(petrinetState, transition.name);
-}
-
-
-petrinetState.sortArc();
-
-let moveableTokens: Array<TokenState> = [];
-// Creation of the tokens in the correct places
-for (let place of petrinetState.getPlaces()) {
-  for (let i = 0; i < place.getCurrentTokenNumber(); i++) {
-    moveableTokens.push(new TokenState(petrinetState, place));
+    console.log();
   }
-}
-  
-// Creation of the events AND tokens movements
-  let triggers: Array<Trigger> = [];
-    for(let event of petrinet.events) {
-        if (isEvolution(event)) {
-          ContinueEvolving(petrinetState, EvolveIt(petrinetState, 1, moveableTokens, triggers), moveableTokens, triggers);
-          petrinetState.addEvent(new EvolutionState(petrinetState, moveableTokens, triggers));
-        } else {
-          continueResetting(petrinetState, petrinetState.getEvents().length, moveableTokens)
-          petrinetState.addEvent(new ResetState(petrinetState, moveableTokens));
-        }
+
+  console.log("------------------------------------------------------------------------");
+
+  console.log("------------------------------------------------------------------------");
+
+  console.log("2 : Trigger");
+  petrinetState.trigger();
+  for (let place of petrinetState.getPlaces()) {
+    console.log();
+    console.log("    Tokens in place " + place.getPlace().name + " : ");
+    for (let i = 0; i < petrinetState.getTokens().length; i++) {
+      if (petrinetState.getTokens()[i].getPosition() == place) {
+        console.log(petrinetState.getTokens()[i] + " , ");
+      }
     }
-
-
-
-console.log("------------------------------------------------------------------------");
-
-console.log("1 : Base");
-console.log("events : " + petrinetState.getEvents());
-
-for (let place of petrinetState.getPlaces()) {
-  console.log();
-  console.log("    Tokens in place " + place.getName() + " : ");
-  for (let i = 0; i < moveableTokens.length; i++) {
-    if (moveableTokens[i].getPosition() == place) {
-      console.log(moveableTokens[i] + " , ");
-    }
+    console.log();
   }
-  console.log();
-}
 
-console.log("------------------------------------------------------------------------");
+  console.log("------------------------------------------------------------------------");
 
-new EvolutionState(petrinetState, moveableTokens);
+  console.log("------------------------------------------------------------------------");
 
-console.log("2 : Evolution");
-console.log("events : " + petrinetState.getEvents());
-
-for (let place of petrinetState.getPlaces()) {
-  console.log();
-  console.log("    Tokens in place " + place.getName() + " : ");
-  for (let i = 0; i < moveableTokens.length; i++) {
-    if (moveableTokens[i].getPosition() == place) {
-      console.log(moveableTokens[i] + " , ");
+  console.log("3 : Trigger");
+  petrinetState.trigger();
+  for (let place of petrinetState.getPlaces()) {
+    console.log();
+    console.log("    Tokens in place " + place.getPlace().name + " : ");
+    for (let i = 0; i < petrinetState.getTokens().length; i++) {
+      if (petrinetState.getTokens()[i].getPosition() == place) {
+        console.log(petrinetState.getTokens()[i] + " , ");
+      }
     }
+    console.log();
   }
-  console.log();
-}
 
-console.log("------------------------------------------------------------------------");
-
-new ResetState(petrinetState, moveableTokens);
-console.log("3 : Reset");
-console.log("events : " + petrinetState.getEvents());
-
-for (let place of petrinetState.getPlaces()) {
-  console.log();
-  console.log("    Tokens in place " + place.getName() + " : ");
-  for (let i = 0; i < moveableTokens.length; i++) {
-    if (moveableTokens[i].getPosition() == place) {
-      console.log(moveableTokens[i] + " , ");
-    }
-  }
-  console.log();
-}
-
-console.log("------------------------------------------------------------------------");
+  console.log("------------------------------------------------------------------------");
 }
 
 
