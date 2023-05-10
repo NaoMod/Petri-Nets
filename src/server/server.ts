3b2b6fa693daff19b60bc8641324770b9bd986b5
Lena BAMOUH <lenabamoh@gmail.com>
2023-05-10 11:38:21 +0200

    Upgrading "for" loop

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,7 +1,6 @@
 import { Server } from 'jayson';
 import { run } from '../main';
 
-
 export function startServer(port: number): void {
     const server: Server = new Server({
         'execute': execute
@@ -12,6 +11,7 @@ export function startServer(port: number): void {
 
 function execute(fileNames: string[], callback: any): void {
     if (fileNames.length == 0) throw new Error("No files in this folder.");
-    for (let i = 0; i < fileNames.length; i++)
-        callback(null, run(fileNames[i]));
+    for (let fileName of fileNames) {
+        callback(null, run(fileName));
+    }
 }
