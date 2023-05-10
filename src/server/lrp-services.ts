23938d6b85cd520fb3c79fe916affc77360daf54
Lena BAMOUH <lenabamoh@gmail.com>
2023-05-10 11:38:01 +0200

    Change of ModelElement implementation

--- src/server/lrp-services.ts
+++ src/server/lrp-services.ts
@@ -1,5 +1,5 @@
 import { NodeFileSystem } from "langium/node";
-import { PetriNet, Edge } from "src/generated/ast";
+import { PetriNet, Place, Transition } from "src/generated/ast";
 import { extractAstNode } from "src/parse-util";
 import { createPetriNetServices } from "src/petri-net-module";
 import { CheckBreakpointArguments, CheckBreakpointResponse, GetBreakpointTypesResponse, GetRuntimeStateArguments, InitArguments, InitResponse, ParseArguments, ParseResponse, LRPServices, StepArguments, StepResponse, ModelElement, Location } from "./lrp";
@@ -17,7 +17,15 @@ class PetriNetModelElement implements ModelElement {
     constructor(petrinet: PetriNet) {
         this.id = petrinet.name;
         this.type = petrinet.$type;
-        this.children = { "places": new PlaceModelElement(petrinet), "transitions": new TransitionModelElement(petrinet) };
+        let modelPlaces: ModelElement[] = [];
+        let modelTransitions: ModelElement[] = [];
+        for (let place of petrinet.places) {
+            modelPlaces.push(new PlaceModelElement(place));
+        }
+        for (let transition of petrinet.transitions) {
+            modelTransitions.push(new TransitionModelElement(transition));
+        }
+        this.children = { "places": modelPlaces, "transitions": modelTransitions };
         this.refs = {};
         this.attributes = { name: petrinet.name };
     }
@@ -31,23 +39,14 @@ class PlaceModelElement implements ModelElement {
     attributes: { [key: string]: any; };
     location?: Location | undefined;
 
-    constructor(petrinet: PetriNet) {
-        this.id = petrinet.name;
-        this.type = "Place[]";
+    constructor(place: Place) {
+        this.id = place.name;
+        this.type = place.$type;
         this.children = {};
         this.refs = {};
-        let placesNames: string[] = [];
-        let placesCapacities: number[] = [];
-        let placesInitialTokenNumber: number[] = [];
-        for (let place of petrinet.places) {
-            placesNames.push(place.name);
-            placesCapacities.push(place.maxCapacity);
-            placesInitialTokenNumber.push(place.initialTokenNumber);
-        }
-        this.attributes = { placesNames: [placesCapacities, placesInitialTokenNumber] };
+        this.attributes = { placeCapacity: place.maxCapacity, placeInitTokenNumber: place.initialTokenNumber };
     }
 }
-
 
 class TransitionModelElement implements ModelElement {
     id: string;
@@ -57,20 +56,12 @@ class TransitionModelElement implements ModelElement {
     attributes: { [key: string]: any; };
     location?: Location | undefined;
 
-    constructor(petrinet: PetriNet) {
-        this.id = petrinet.name;
-        this.type = "Transition[]";
+    constructor(transition: Transition) {
+        this.id = transition.name;
+        this.type = transition.$type;
         this.children = {};
-        let transitionNames: string[] = [];
-        let transitionSources: (Edge[])[] = [];
-        let transitionDestinations: (Edge[])[] = [];
-        for (let transition of petrinet.transitions) {
-            transitionNames.push(transition.name);
-            transitionSources.push(transition.sources)
-            transitionDestinations.push(transition.destinations)
-        }
-        this.attributes = { names: transitionNames };
-        this.refs = { transitionNames: [transitionSources.toString(), transitionDestinations.toString()] };
+        this.attributes = {};
+        this.refs = { sources: transition.sources.toString(), destinations: transition.destinations.toString() };
     }
 }
 
@@ -80,56 +71,54 @@ export class PetriNetsLRPServices implements LRPServices {
     petrinetsState = new Map<string, PetriNetState>();
 
     async parse(args: ParseArguments): Promise<ParseResponse> {
+        this.petrinetsState.delete(args.sourceFile);
+
         const services = createPetriNetServices(NodeFileSystem).PetriNet;
         let petrinet = await extractAstNode<PetriNet>(args.sourceFile, services);
         this.petrinets.set(args.sourceFile, petrinet);
 
         return { astRoot: new PetriNetModelElement(petrinet) };
     }
+
     initExecution(args: InitArguments): InitResponse {
-        if (this.petrinets.has(args.sourceFile)) {
-            let petrinet = this.petrinets.get(args.sourceFile);
-            if (petrinet != undefined) {
-                this.petrinetsState.set(args.sourceFile, new PetriNetState(petrinet, 50));
-                return { isExecutionDone: true };
-            } else {
-                throw new Error("The petri net of this file is undefined.");
-            }
-        } else {
+        if (!this.petrinets.has(args.sourceFile))
             throw new Error("The petri net of this file has not been parsed yet.");
-        }
+
+        let petrinet = this.petrinets.get(args.sourceFile);
+        if (petrinet == undefined)
+            throw new Error("The petri net of this file is undefined.");
+
+        this.petrinetsState.set(args.sourceFile, new PetriNetState(petrinet, 50));
+        return { isExecutionDone: true };
     }
+
     getRuntimeState(args: GetRuntimeStateArguments): GetRuntimeStateArguments {
-        if (this.petrinetsState.has(args.sourceFile)) {
-            let petrinetState = this.petrinetsState.get(args.sourceFile)
-            if (petrinetState != undefined) {
-                console.log("Current State : ");
-                for (let place of petrinetState.getPlaces()) {
-                    console.log();
-                    console.log("    Tokens in place " + place.getPlace().name + " : " + place.getCurrentTokenNumber());
-                    console.log();
-                }
-            } else {
-                throw new Error("The runtime state of this file is undefined.")
-            }
-        } else {
-            throw new Error("The runtime state of this file has not been initialized yet.")
+        if (!this.petrinetsState.has(args.sourceFile))
+            throw new Error("The runtime state of this file has not been initialized yet.");
+
+        let petrinetState = this.petrinetsState.get(args.sourceFile)
+        if (petrinetState == undefined)
+            throw new Error("The runtime state of this file is undefined.");
+
+        console.log("Current State : ");
+        for (let place of petrinetState.getPlaces()) {
+            console.log();
+            console.log("    Tokens in place " + place.getPlace().name + " : " + place.getCurrentTokenNumber());
+            console.log();
         }
         return { sourceFile: args.sourceFile };
     }
 
     nextStep(args: StepArguments): StepResponse {
-        if (this.petrinetsState.has(args.sourceFile)) {
-            let petrinetState = this.petrinetsState.get(args.sourceFile);
-            if (petrinetState != undefined) {
-                petrinetState.trigger();
-                return { isExecutionDone: petrinetState.canEvolve() };
-            } else {
-                throw new Error("The runtime state of this file is undefined.")
-            }
-        } else {
-            throw new Error("The runtime state of this file has not been initialized yet.")
-        }
+        if (!this.petrinetsState.has(args.sourceFile))
+            throw new Error("The runtime state of this file has not been initialized yet.");
+
+        let petrinetState = this.petrinetsState.get(args.sourceFile);
+        if (petrinetState == undefined)
+            throw new Error("The runtime state of this file is undefined.");
+
+        petrinetState.trigger();
+        return { isExecutionDone: petrinetState.canEvolve() };
     }
 
     getBreakpointTypes(): GetBreakpointTypesResponse {
