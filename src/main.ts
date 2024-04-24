516d15d046919f5583d0432e34664b45304b31e7
joss-enet <enet.josselin@gmail.com>
2024-04-24 22:11:17 +0200

    Remove generator

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,4 @@
-import path from 'path';
-import { generatePetriNetFile } from './petrinet-generator';
 import { LRPServer } from './server/server';
-
 
 export default async function main() {
   const { Command } = require('commander');
@@ -18,39 +15,6 @@ export default async function main() {
       await new Promise<void>(resolve => setTimeout(() => {
         resolve()
       }, 2000));
-    });
-
-  program.command('generate')
-    .description('Generate a .PetriNet file')
-    .option('-p, --places <number>[:<number>]', 'Generate a .PetriNet file with a specific number of places or with a random number of places between a min and a max')
-    .action((options: any) => {
-      const generatedDirectoryPath = path.join(__dirname, '../examples/generated');
-
-      if (options.places) {
-        let secondNumber: boolean = false;
-        let minNumber: string = '0';
-        let maxNumber: string = '0';
-        for (let i = 0; i < options.places.length; i++) {
-          if (options.places[i] == ':') {
-            if (i == options.places.length - 1)
-              throw Error('Incorrect command');
-            secondNumber = true;
-            i++;
-          }
-          if (!secondNumber)
-            minNumber = minNumber + options.places[i];
-          else
-            maxNumber = maxNumber + options.places[i];
-        }
-        if (Number(maxNumber) == 0)
-          generatePetriNetFile(generatedDirectoryPath + "/specificNumberPlacesGenerated.PetriNet", generatedDirectoryPath, undefined, Number(minNumber));
-        else if (Number(minNumber) > Number(maxNumber))
-          throw Error('Maximum must be greater than minimum');
-        else
-          generatePetriNetFile(generatedDirectoryPath + "/randomMinMaxNumberPlacesGenerated.PetriNet", generatedDirectoryPath, undefined, undefined, Number(minNumber), Number(maxNumber));
-      }
-      else
-        generatePetriNetFile(generatedDirectoryPath + "/randomGenerated.PetriNet", generatedDirectoryPath);
     });
 
   try {
